0e3f3a1148c1ddaf290b8b3c7cd7d87502b50540
JackTLi <jt3li@uwaterloo.ca>
2016-01-03 15:37:23 -0500

    Finished for today

--- js/app.js
+++ js/app.js
@@ -29,10 +29,29 @@ $(function() {
     
     $(window).scroll( function(){
         
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        //Nav background
+        if ($(window).scrollTop() > 700)
+        {
+            if (!$('.nav').hasClass('nav-background'))
+            {
+                $('.nav').addClass('nav-background');
+            }
+        }
+        
+        if ($(window).scrollTop() < 600)
+        {
+            if ($('.nav').hasClass('nav-background'))
+            {
+                $('.nav').removeClass('nav-background');
+            }
+        }
+
+
         //Fadein on scroll
         $('.fadein').each( function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();   
             
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
@@ -46,7 +65,6 @@ $(function() {
         //Slide-in (left) on scroll
         $('.slidein-left').each( function(i){
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
@@ -60,7 +78,6 @@ $(function() {
         //Slide-in (right) on scroll
         $('.slidein-right').each( function(i){
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
@@ -70,10 +87,13 @@ $(function() {
                     
             }
         }); 
-
-
     
     });
+
+    $(".sliding-link").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 400);
+    } );
 
     function initHeader() {
         width = $(window).width();
